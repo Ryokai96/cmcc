488e6d083f692e3d5aaa618dc42d6ffb01bde1c5
ryoukai <894175329@qq.com>
2022-11-26 12:01:11 +0800

    telInfo

--- src/bin/telInfo.js
+++ src/bin/telInfo.js
@@ -1,14 +1,18 @@
-var axios = require('axios').default
-var cheerio = require('cheerio')
+var cheerio = require('cheerio');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
-axios.get('https://shouji.hwcha.com/18870968244')
-  .then(function(response) {
-    console.log();
-    getData(response.data)
-  })
-
-function getData(html) {
-  var $ = cheerio.load(html)
-  var $pList = $('div .container .panel-body p')
-  console.log($pList);
+let ajax = new XMLHttpRequest();
+let html;
+ajax.open('GET', 'https://shouji.hwcha.com/18870968244', false);
+ajax.onreadystatechange = () => {
+  if (ajax.readyState === 4 && ajax.status === 200) {
+    html = ajax.responseText
+  }
 }
+ajax.send();
+const $ = cheerio.load(html);
+const anchor = $('.container .panel-body p');
+anchor.each(function() {
+  let text = $(this).html()
+  console.log(text)
+})
